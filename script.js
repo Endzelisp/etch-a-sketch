ff1080db9cbdc5dbd664f647aac7bcbef789b424
Armando Endzelis <endzelisp@gmail.com>
2022-09-02 17:44:47 -0400

    Create function that will generate the square

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const containerEl = document.querySelector('.container');
+const containerEl = document.querySelector('.cells-container');
 
 let gridSizeEl = document.querySelector(':root');
 
-gridSizeEl.style.setProperty('--rows-and-columns', 5)
+let numOfCells = 100;
+
+let gridSize = Math.sqrt(numOfCells)
+
+gridSizeEl.style.setProperty('--rows-and-columns', gridSize)
+
+function createElem (elem, numOfElem) {
+let newEl;
+  for (let i = 0; i < numOfElem; ++i) {
+  newEl = document.createElement(elem)
+  newEl.classList.add('cells-bg-color')
+  containerEl.appendChild(newEl)
+  }
+  
+}
+
+createElem('div', numOfCells)
 
